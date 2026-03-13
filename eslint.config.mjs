d6d3781531387e1c5cf814a1d5e4504ba613243d
kapsarovL <kapsarovlazar@gmail.com>
2026-03-13 23:04:47 +0100

    chore: apply prettier formatting to all source files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import nextPlugin from "@next/eslint-plugin-next";
-import prettierConfig from "eslint-config-prettier";
-import reactCompiler from "eslint-plugin-react-compiler";
-import globals from "globals";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
+import prettierConfig from 'eslint-config-prettier';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import globals from 'globals';
 
 export default tseslint.config(
   {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
     plugins: {
-      "@next/next": nextPlugin,
+      '@next/next': nextPlugin,
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
     },
   },
   prettierConfig,
@@ -29,10 +29,10 @@ export default tseslint.config(
       },
     },
     plugins: {
-      "react-compiler": reactCompiler,
+      'react-compiler': reactCompiler,
     },
     rules: {
-      "react-compiler/react-compiler": "error",
+      'react-compiler/react-compiler': 'error',
     },
   },
 );
