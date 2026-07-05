27aaecdb70b5d878b5a88bd4fde66a994908c242
Lazar Kapsarov <kapsarovlazar@gmail.com>
2026-07-05 23:48:07 +0200

    fix: remove unused idx parameter from featured-products map (#168)

--- components/home/featured-products.tsx
+++ components/home/featured-products.tsx
@@ -23,7 +23,7 @@ export async function FeaturedProducts() {
         </div>
 
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-          {featured.map((product, idx) => {
+          {featured.map(product => {
             const imageUrl = (product.images as { url: string }[] | null)?.[0]?.url ?? null;
             return (
               <Link key={product.id} href={`/products/${product.slug}`} className="group flex flex-col gap-3">
